fb3e54794db29ee34f71da1969913f2f03f833fa
Abishek0612 <uabishek6@gmail.com>
2025-09-12 23:12:03 +0530

    Add Netlify redirects for SPA routing

--- src/config/passport.js
+++ src/config/passport.js
@@ -10,7 +10,10 @@ export const configurePassport = () => {
         {
           clientID: process.env.GOOGLE_CLIENT_ID,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          callbackURL: "/api/auth/google/callback",
+          callbackURL: `${
+            process.env.BASE_URL ||
+            "https://chat-application-server-dof5.onrender.com"
+          }/api/auth/google/callback`,
         },
         async (accessToken, refreshToken, profile, done) => {
           try {

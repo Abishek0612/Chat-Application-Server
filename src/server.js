15ba239e7e42820bd30a7b78d0337bcc4cde863a
Abishek0612 <uabishek6@gmail.com>
2025-09-12 22:28:41 +0530

    add public folder with google verification file

--- src/server.js
+++ src/server.js
@@ -96,7 +96,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Serve uploaded files statically
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use(
